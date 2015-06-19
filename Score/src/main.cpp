8a8549109ccd904dfe7e065c16ce62fdaabd460a
U-PS14B22\vantan <akira206@gmail.com>
2015-06-20 07:42:47 +0900

    Score新規

---
+++ Score/src/main.cpp
@@ -0,0 +1,19 @@
+﻿
+#include "lib/framework.hpp"
+
+
+int main() {
+  AppEnv env(640, 480);
+  env.bgColor(Color(0.8f, 0.8f, 0.8f));
+
+  Font font("res/MeiryoConsolas.ttf", 2);
+  font.size(100);
+
+  while (env.isOpen()) {
+    env.begin();
+    
+    font.draw("aaa", Vec2f::Zero(), Color::white);
+
+    env.end();
+  }
+}
